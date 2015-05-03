c8ec3dee7a7414247b6db7c50220a8fb0fb078b6
Calv Collins <calvcoll@gmail.com>
2015-05-03 16:06:25 +0100

    initial version

---
+++ openings.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+type videoName struct {
+	Title  string
+	Source string
+}
+
+type videoInfo struct {
+	Success    bool
+	Videourl   string
+	Videoname  videoName
+	Videofname string
+}
+
+var homeURL = "http://openings.moe/"
+var client = &http.Client{}
+var wg sync.WaitGroup
+
+func makeVideoAndSend(cs chan videoInfo) {
+	req, _ := http.NewRequest("GET", (homeURL + "nextvideo.php"), nil)
+	req.Header.Add("Host", "openings.moe")
+	req.Header.Add("Referer", homeURL)
+	resp, _ := client.Do(req)
+	var body []byte
+	if resp != nil {
+		body, _ = ioutil.ReadAll(resp.Body)
+	}
+	// response := string(body)
+	// fmt.Println(response)
+	var video videoInfo
+	err := json.Unmarshal(body, &video)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	cs <- video
+}
+
+func recieveVideoAndSave(cs chan videoInfo) {
+	// needed because otherwise main will close before these are done.
+	defer wg.Done()
+	video := <-cs
+	if video.Success {
+		out, _ := os.Create(video.Videofname)
+		defer out.Close()
+		req, _ := http.NewRequest("GET", homeURL+video.Videourl, nil)
+		req.Header.Add("Host", "openings.moe")
+		req.Header.Add("Referer", homeURL)
+		fileRequest, _ := client.Do(req)
+		var file []byte
+		if fileRequest != nil {
+			file, _ = ioutil.ReadAll(fileRequest.Body)
+		}
+		out.Write(file)
+		fmt.Printf("%+v has been successfully downloaded!\n", video.Videofname)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+
+	j := 5
+	if !reflect.DeepEqual(args, make([]string, 0)) {
+		j64, _ := strconv.ParseInt(args[0], 10, 32) //sets j to be amount specified by args.
+		j = int(j64)
+	}
+	cs := make(chan videoInfo)
+
+	for i := 0; i < j; i++ {
+		wg.Add(1)
+		go makeVideoAndSend(cs)
+		go recieveVideoAndSave(cs)
+	}
+	wg.Wait()
+}
